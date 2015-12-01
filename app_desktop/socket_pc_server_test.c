bbc399a67540e021b7cdf3e7c740409a8c6ebaa8
Hyeondo Jung <jsmjhd@naver.com>
2015-12-01 01:40:53 -0800

    memo connect

--- app_desktop/socket_pc_server_test.c
+++ app_desktop/socket_pc_server_test.c
@@ -27,7 +27,7 @@ int main()
         memset(&server_addr, 0, sizeof(server_addr));
         server_addr.sin_family = AF_INET;
         server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-        server_addr.sin_port = htons(3317);
+        server_addr.sin_port = htons(9999);
        
     //binding
    if(bind(ssock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
@@ -49,9 +49,9 @@ int main()
         while(1)
         {     csock = accept(ssock, (struct sockaddr *)&client_addr, &clen);
                i++;
-              
-               if(write(csock, buf, MAXBUF) <= 0)
-                       perror("Writing error : ");
+              printf("\ncome here\n");
+               //if(write(csock, buf, MAXBUF) <= 0)
+                 //      perror("Writing error : ");
                       
                if(read(csock, readbuf, MAXBUF) < 0)
                        perror("Reading error : ");
