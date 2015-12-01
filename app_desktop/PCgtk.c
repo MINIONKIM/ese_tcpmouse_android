39dac0f8513f758564bd96fc9751de09830f7808
Yunsoyeon <thdus9094@gmail.com>
2015-12-01 02:38:49 -0800

    r

--- app_desktop/PCgtk.c
+++ app_desktop/PCgtk.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 
+const gchar *state_text;
+const gchar *data_text;
 static GtkWidget *xpm_label_box( gchar *xpm_filename, gchar *label_text )
 {
     GtkWidget *box;
@@ -27,16 +29,45 @@ static GtkWidget *xpm_label_box( gchar *xpm_filename, gchar *label_text )
 
     return box;
 }
-static void enter_callback (GtkWidget *widget, GtkWidget *entry)
+
+static void enter1_callback (GtkWidget *widget, GtkWidget *entry)
 {
-	const gchar *entry_text;
-	entry_text = gtk_entry_get_text (GTK_ENTRY(entry));
-	printf("Entry contents : %s\n", entry_text);
+	state_text = gtk_entry_get_text (GTK_ENTRY(entry));
+	printf("Entry contents : %s\n", state_text);
+	
+	return;
 }
 
-static void button_callback(GtkWidget *widget, gpointer data)
+static void enter2_callback (GtkWidget *widget, GtkWidget *entry)
 {
-	g_print("Hello again - %s was pressed\n", (char *) data);
+	data_text = gtk_entry_get_text (GTK_ENTRY(entry));
+	printf("Entry contents : %s\n", data_text);
+
+	return;
+}
+
+static void on_button1_clicked(GtkButton* button, gpointer data)
+{
+    /* cast the data back to a char*  */
+    char* txt = (char*)data;
+    
+    printf("open_button_clicked - '%s'\n", txt);
+    fflush(stdout);
+}
+
+static void on_button2_clicked(GtkButton* button, gpointer data)
+{
+    /* cast the data back to a char*  */
+    char* txt = (char*)data;
+    printf("close_button_clicked - '%s'\n", txt);
+    fflush(stdout);
+}
+
+/* "quit" button "clicked" callback function. */
+static void on_quit_clicked(GtkButton* button, gpointer data)
+{
+    /* make sure we realy quit. */
+    gtk_main_quit();
 }
 
 int main (int argc, char *argv[])
@@ -50,6 +81,7 @@ int main (int argc, char *argv[])
     /* this will store push buttons */
     GtkWidget *button1;
     GtkWidget *button2;
+    GtkWidget *button3;
     /* this will store a horizontal box*/
     GtkWidget *hbox;
 
@@ -68,7 +100,7 @@ int main (int argc, char *argv[])
     /* create a new window */
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title (GTK_WINDOW (window), "PC GTK Entry");
-    gtk_widget_set_size_request(GTK_WIDGET (window), 400, 200);
+    gtk_widget_set_size_request(GTK_WIDGET (window), 200, 350);
     gtk_container_set_border_width(GTK_CONTAINER (window), 5);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
 
@@ -93,12 +125,14 @@ int main (int argc, char *argv[])
     gtk_box_pack_start (GTK_BOX (hbox), vbox, FALSE, FALSE, 0);
     gtk_container_set_border_width (GTK_CONTAINER (window), 5);
 
-    frame = gtk_frame_new("IP Address");
+    frame = gtk_frame_new("STATE");
+
     //entry ..	
     entry = gtk_entry_new ();
     gtk_entry_set_max_length (GTK_ENTRY (entry), 100);
-    g_signal_connect (entry, "activate",G_CALLBACK (enter_callback), entry);
-    gtk_entry_set_text (GTK_ENTRY (entry), "Put your IP address");
+    gtk_widget_set_size_request(entry, 170, 50);
+    g_signal_connect (entry, "activate",G_CALLBACK (enter1_callback), entry);
+    gtk_entry_set_text (GTK_ENTRY (entry), "Disconnect");
 
     tmp_pos = GTK_ENTRY (entry)->text_length;
     gtk_editable_select_region (GTK_EDITABLE (entry),0, 
@@ -107,12 +141,14 @@ int main (int argc, char *argv[])
     gtk_box_pack_start (GTK_BOX (vbox), frame, FALSE, FALSE, 0);
     gtk_widget_show (entry);
 
-   frame = gtk_frame_new("PORT");
-   //entry2 ..	
+    frame = gtk_frame_new("DATA");
+
+    //entry2 ..	
     entry2 = gtk_entry_new ();
     gtk_entry_set_max_length (GTK_ENTRY (entry2), 100);
-    g_signal_connect (entry2, "activate",G_CALLBACK (enter_callback), entry2);
-    gtk_entry_set_text (GTK_ENTRY (entry2), "Put your Port number");
+     gtk_widget_set_size_request(entry2, 170, 50);
+    g_signal_connect (entry2, "activate",G_CALLBACK (enter2_callback), entry2);
+    gtk_entry_set_text (GTK_ENTRY (entry2), "What data");
 
     tmp_pos = GTK_ENTRY (entry2)->text_length;
     gtk_editable_select_region (GTK_EDITABLE (entry2),0, 
@@ -123,19 +159,41 @@ int main (int argc, char *argv[])
 
    
     //button ...
-    //button = gtk_button_new_with_label("Connect");
-    //gtk_signal_connect (GTK_OBJECT (button), "clicked", 
-	//	GTK_SIGNAL_FUNC (button_callback), NULL);
+    button1 = gtk_button_new_with_label("Open");
+   
 
     //gtk_signal_connect_object (GTK_OBJECT (button), "clicked", 
 	//	GTK_SIGNAL_FUNC (gtk_widget_destory), 
 	//	GTK_OBJECT (window));
     
-    //gtk_container_add (GTK_CONTAINER (window), button);
-    //gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, TRUE, 0);
+    //gtk_container_add (GTK_CONTAINER (window), button1);
+    gtk_widget_set_size_request(button1, 50, 50);
+    gtk_box_pack_start (GTK_BOX (vbox), button1, FALSE, TRUE, 1);
+    
 
-    //gtk_widget_show(button);
+    gtk_signal_connect (GTK_OBJECT (button1), "clicked", 
+		GTK_SIGNAL_FUNC (on_button1_clicked), NULL);
 
+    gtk_widget_show(button1);
+
+
+   // button2
+   button2 = gtk_button_new_with_label("Close");
+   gtk_widget_set_size_request(button2, 50, 50);  
+   gtk_box_pack_start (GTK_BOX (vbox), button2, FALSE, TRUE, 1);
+   gtk_signal_connect (GTK_OBJECT (button2), "clicked", 
+			GTK_SIGNAL_FUNC (on_button2_clicked), NULL);
+
+    gtk_widget_show(button2);
+
+   // button3
+   button3 = gtk_button_new_with_label("Quit");  
+   gtk_widget_set_size_request(button3, 50, 50);
+   gtk_box_pack_start (GTK_BOX (vbox), button3, FALSE, TRUE, 1);
+   gtk_signal_connect (GTK_OBJECT (button3), "clicked", 
+			GTK_SIGNAL_FUNC (on_quit_clicked), NULL);
+
+    gtk_widget_show(button3);
 
     gtk_widget_show_all(window);
 
