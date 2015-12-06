6f5963a8c7c3f780d18f733b20845d114030dd1e
Hyeondo Jung <jsmjhd@naver.com>
2015-12-06 09:30:28 -0800

    lib update

---
+++ app_desktop/lib/MouseEvent/MoveMouse.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+void MoveMouse(char* x,char* y)
+{
+	char* instruction = "xdotool mousemove_relative -- ";
+	char* sp=" ";
+	int i;
+	char inst[50]={0,};
+	printf("z : %s , x : %s \n",x,y);
+	strcat(inst, instruction);
+	strcat(inst, x);
+	strcat(inst, sp);
+	strcat(inst, y);
+	printf("%s\n", inst);
+	usleep(100);
+	for(i=0;i<10;i++) system(inst);
+
+}
